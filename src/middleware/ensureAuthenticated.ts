49d0db3df7e071578c9f714a6630eb69a64bceaf
Vitor Machado <vitormds22@gmail.com>
2021-10-20 01:09:37 -0300

    Finalizando a configuração do envio de menssagens com a autenticação validada

---
+++ src/middleware/ensureAuthenticated.ts
@@ -0,0 +1,37 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+interface IPayload {
+    sub: string
+}
+export function ensureAuthenticated(
+    request: Request, 
+    response: Response, 
+    next: NextFunction
+) {
+    const authToken = request.headers.authorization;
+    
+    if(!authToken) {
+        return response.status(401).json({
+            errorCode: "token.invalid",
+        });
+    }
+
+    // Bearer 6as4d6a4da65d4s
+    // [0]        [1] 
+    const [ , token ] = authToken.split(" ");
+
+    try {
+        
+        const { sub } = verify(token, process.env.JWT_SECRET) as IPayload;
+
+        request.user_id = sub
+
+        return next();
+    } catch(err) {
+        return response.status(401).json({
+            errorCode: "token.expired"
+        })
+    }
+
+}
